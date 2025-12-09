4dee864da1f94261d6fd86808c0a3baaf8a94800
vaishnavichorat <ivaishnavichorat@gmail.com>
2025-12-09 16:31:38 +0530

    Initial commit

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="flex flex-col items-center justify-center h-screen bg-gray-100">
+      <Button>
+        Get Started
+      </Button>
+    </div>
+
+   );
+}
